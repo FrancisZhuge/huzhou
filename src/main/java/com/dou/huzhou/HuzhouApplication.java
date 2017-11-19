4be755747a3c24e0da7d9fbeffeea3fccb210093
francis <franciszhuge@163.com>
2017-11-19 20:42:03 +0800

    第一次创建项目

---
+++ src/main/java/com/dou/huzhou/HuzhouApplication.java
@@ -0,0 +1,12 @@
+package com.dou.huzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuzhouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuzhouApplication.class, args);
+	}
+}
